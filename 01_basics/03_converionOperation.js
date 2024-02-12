f0ebf6f022b2932e214a37649aac743936ab9cb5
Malik Mohammad Rashid <56771670+MalikRashid786@users.noreply.github.com>
2024-02-12 23:28:04 +0530

    conversion

---
+++ 01_basics/03_converionOperation.js
@@ -0,0 +1,18 @@
+let score = "33"
+
+// console.log(typeof score);
+
+let valueNumber = Number(score)
+
+// console.log(valueNumber)
+// console.log(typeof valueNumber)
+
+let value= 22
+
+console.log(typeof value)
+
+let valueString = String(value)
+
+console.log(typeof valueString);
+
+console.log(valueString);
