01fa9269027c49b72541dd347406714112f06865
Malik Mohammad Rashid <56771670+MalikRashid786@users.noreply.github.com>
2024-02-12 22:54:45 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 45125623
+let accountEmail = "user1@gmail.com"
+var accountPassword = "12345"
+acountCity = "Noida"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, acountCity])
