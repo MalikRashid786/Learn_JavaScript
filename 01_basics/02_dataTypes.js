3cbbc9c35e5b2d58c271f4f23d33538cc420b706
Malik Mohammad Rashid <56771670+MalikRashid786@users.noreply.github.com>
2024-02-12 23:13:58 +0530

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all  JS Code as newer version
+
+let userName= "xyz"
+let phoneNumber= 74512031456
+let loggedIn= false
+
+// dataTypes
+// number
+// bigint
+// string
+// boolean
+//  null => standalone value
+// undefined =>
+// symbol => unique 
+
+console.log(typeof null); // obejct
+console.log(typeof undefined);  // undefined
